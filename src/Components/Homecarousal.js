2ab1871911cd1d491d74e56ea465dfa934a9c866
Krishna Dolas <kdolas99@gmail.com>
2025-12-05 15:20:00 +0530

    Some message

--- src/Components/Homecarousal.js
+++ src/Components/Homecarousal.js
@@ -60,7 +60,7 @@ function HomeCarousel() {
               </h1>
             </div>
             <a
-              href="https://drive.google.com/file/d/1XhZRq-MXxStYCfrAEkP1vnQvwOpST2fW/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1vZpDxSwjrnzkF_4CknYusrqFN9XFTzrO/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
